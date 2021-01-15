ea8f653c0558612edd33783b4bded7e9fc7f064c
rameshsz <61559619+rameshsz@users.noreply.github.com>
2021-01-15 00:47:49 -0600

    aa
    aa

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.googlemaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        <fragment
+        android:layout_width="match_parent"
+
